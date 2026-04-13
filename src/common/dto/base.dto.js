53bdccdc1faea7bd663e05311d8ba89067bb4089
parthmunjal07 <parth.munjal07@gmail.com>
2026-04-13 14:49:47 +0530

    feat: update project structure and add new features
    - Changed the entry point for development from index.js to src/index.js.
    - Added new dependencies: bcrypt, cookie-parser, cors, jsonwebtoken, nodemon, and pg.
    - Created a new HTML file for the frontend interface with Tailwind CSS styling.
    - Implemented a basic Express server in index.mjs with PostgreSQL integration for seat booking.
    - Added API error handling utility (ApiError) for standardized error responses.
    - Added API response utility (ApiResponse) for standardized success responses.
    - Refactored base DTO class to improve validation logic.

--- src/common/dto/base.dto.js
+++ src/common/dto/base.dto.js
@@ -1,18 +1,17 @@
 import Joi from "joi";
-
-export class BaseDto {
+class BaseDto {
     static schema = Joi.object({})
-
-    static validate(data) {
-        const {error, value} = this.schema.validate(data, {
+    static validate(data){
+       const {error, value} =  this.schema.validate(data, {
             abortEarly: false,
             stripUnknown: true
         })
-
-        if (error) {
-            const errors = error.details.map(det => det.message)
+        if(error){
+            const errors = error.details.map((d) => d.message)
             return {errors, value: null}
         }
         return {errors: null, value}
     }
 }
+
+export default BaseDto
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+const app = express()
+app.use(express.json())
+
+export default app
