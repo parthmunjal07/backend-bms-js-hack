74a2ba4de2338dc6f65050938284524ca540caba
parthmunjal07 <parth.munjal07@gmail.com>
2026-04-14 00:17:12 +0530

    feat: add validation middleware and enhance auth controller with refresh token handling

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -3,10 +3,18 @@ import * as authService from './auth.service.js';
 
 const register = async (req, res) => {
     const user = await authService.register(req.body.name, req.body.email, req.body.password);
+
     ApiResponse.created(res, "Registration success", user)
 }
 
 const login = async (req, res) => {
     const user = await authService.login(req.body.email, req.body.password)
+
+    res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     ApiResponse.ok(res, "Login success", user)
 }
