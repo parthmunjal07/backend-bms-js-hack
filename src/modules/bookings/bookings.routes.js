238c246b1142a9c5be6632f947d556106cb72551
parthmunjal07 <parth.munjal07@gmail.com>
2026-04-14 17:41:47 +0530

    refinement

--- src/modules/bookings/bookings.routes.js
+++ src/modules/bookings/bookings.routes.js
@@ -4,11 +4,8 @@ import authenticate from "../auth/auth.middlewares.js";
 
 const bookingRouter = Router();
 
-// Public route: Anyone can see seats
 bookingRouter.get("/seats", bookingController.getSeats);
 
-// Protected route: Only logged in users can book
-// Notice we added the 'authenticate' middleware here!
 bookingRouter.put("/:id", authenticate, bookingController.bookSeat);
 
 export default bookingRouter;
